3f322478da40130c43ad760c3cca0e6af00980d8
Chemaclass <chemaclass@outlook.es>
2025-05-18 17:58:38 +0200

    refactor providerGameRunner()

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -23,17 +23,22 @@ final class GameTest extends TestCase
     {
         foreach (range(1, 3) as $seed) {
             $filename = sprintf(__DIR__.'/tmp/runner-%s.txt', $seed);
-            if (file_exists($filename)) {
-                $content = file_get_contents($filename);
-            } else {
-                srand($seed);
-                ob_start();
-                require __DIR__.'/../GameRunner.php';
-                $content = ob_get_clean();
-                file_put_contents($filename, $content);
+            if (!file_exists($filename)) {
+                self::createSnapshot($seed, $filename);
             }
+
+            $content = file_get_contents($filename);
 
             yield [$seed, $content];
         }
+    }
+
+    private static function createSnapshot(int $seed, string $filename): void
+    {
+        srand($seed);
+        ob_start();
+        require __DIR__.'/../GameRunner.php';
+        $content = ob_get_clean();
+        file_put_contents($filename, $content);
     }
 }
